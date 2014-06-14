e9ad5744e1982bc36ffefb30adb57d08656be927
Thomas Brus <thomas.brus@me.com>
2014-06-14 23:49:34 +0200

    Implemented IntegerLiteral#evaluate

---
+++ src/main/java/interpres/ast/IntegerLiteral.java
@@ -0,0 +1,17 @@
+package interpres.ast;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntegerLiteral extends AST {
+  private Integer value;
+
+  public IntegerLiteral(Integer value) {
+    this.value = value;
+  }
+
+  public List<Object> evaluate() {
+    return Arrays.asList("LOADL " + this.value);
+  }
+}
+
