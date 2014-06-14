6d62673d81a8fc3f0137b8033879a36243dcf3ef
Thomas Brus <thomas.brus@me.com>
2014-06-14 23:51:16 +0200

    Implemented StringLiteral#evaluate

---
+++ src/main/java/interpres/ast/StringLiteral.java
@@ -0,0 +1,22 @@
+package interpres.ast;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class StringLiteral extends AST {
+  private String literal;
+
+  public StringLiteral(String literal) {
+    this.literal = literal;
+  }
+
+  public List<Object> evaluate() {
+    List<Object> instructions = new ArrayList<Object>();
+
+    for (int i = literal.length() - 1; i >= 0; i--) {
+      instructions.add("LOADL " + (int)literal.charAt(i));
+    }
+
+    return instructions;
+  }
+}
