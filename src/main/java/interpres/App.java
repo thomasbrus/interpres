e2904886d0e7f5c6c7e70733f20cc4673ba53fc0
Thomas Brus <thomas.brus@me.com>
2014-06-22 16:43:06 +0200

    Fixed lengthy main-method in App

--- src/main/java/interpres/App.java
+++ src/main/java/interpres/App.java
@@ -4,6 +4,9 @@ import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.function.BiFunction;
@@ -14,97 +17,55 @@ import interpres.ast.QuotedExpression;
 import interpres.ast.ListExpression;
 import interpres.ast.Symbol;
 
+import interpres.definitions.DefinitionTable;
+import interpres.instructions.PrintableInstructionSequence;
+
 public class App {
-  @SuppressWarnings("unchecked")
-  public static void main(String[] args) throws IOException, RecognitionException {
+  private DefinitionTable definitionTable;
+
+  public App(DefinitionTable definitionTable) {
+    this.definitionTable = definitionTable;
+  }
+
+  public PrintableInstructionSequence evaluate(InputStream inputStream) throws IOException, RecognitionException {
+    AST ast = this.transform(this.parse(inputStream));
+    return ast.evaluate(this.definitionTable);
+  }
+
+  private CommonTree parse(InputStream inputStream) throws IOException, RecognitionException {
     // Create a token stream
-    GrammarLexer lexer = new GrammarLexer(new ANTLRInputStream(System.in));
+    GrammarLexer lexer = new GrammarLexer(new ANTLRInputStream(inputStream));
     CommonTokenStream tokens = new CommonTokenStream(lexer);
 
     // Parse the input using the token stream
     GrammarParser parser = new GrammarParser(tokens);
-    CommonTree tree = (CommonTree)parser.parse().getTree();
+    return (CommonTree) parser.parse().getTree();
+  }
 
-    System.out.println("ANTLR syntax tree:");
-    System.out.println(tree.toStringTree());
-
-    // Transform the ANTLR tree into a custom AST classes
+  private AST transform(CommonTree tree) throws RecognitionException {
+    // Transform the ANTLR tree into an Interpres AST
     CommonTreeNodeStream nodeStream = new CommonTreeNodeStream(tree);
     TreeWalker walker = new TreeWalker(nodeStream);
     TreeWalker.walk_return walkReturn = walker.walk();
 
-    // Setup the definition table
+    return walkReturn.ast;
+  }
+
+  public static void main(String[] args) throws IOException, RecognitionException {
     DefinitionTable definitionTable = new DefinitionTable();
 
-    definitionTable.define("define", (BiFunction<DefinitionTable, List<AST>, Object>) (dt, arguments) -> {
-      Symbol symbol = (Symbol) arguments.get(0);
-      definitionTable.define(symbol.getName(), arguments.get(1).evaluate(dt));
-      return null;
-    });
+    definitionTable.define(new interpres.definitions.core.Define());
+    definitionTable.define(new interpres.definitions.core.Lambda());
+    definitionTable.define(new interpres.definitions.core.Quote());
+    definitionTable.define(new interpres.definitions.core.Unquote());
+    definitionTable.define(new interpres.definitions.core.Concat());
+    definitionTable.define(new interpres.definitions.core.Repeat());
+    definitionTable.define(new interpres.definitions.core.Length());
 
-    definitionTable.define("quote", (BiFunction<DefinitionTable, List<AST>, Object>) (DefinitionTable dt, List<AST> arguments) ->
-      arguments.get(0)
-    );
-    // (define println (lambda (string-instructions)
-    //   (core.concat
-    //     string-instructions
-    //     (core.repeat (core.length string-instructions) "CALL put"))))
+    App app = new App(definitionTable);
 
-    definitionTable.define("unquote", (BiFunction<DefinitionTable, List<AST>, Object>) (DefinitionTable dt, List<AST> arguments) ->
-      ((QuotedExpression) arguments.get(0)).evaluate(dt).evaluate(dt)
-    );
-    definitionTable.define("lambda", (BiFunction<DefinitionTable, List<AST>, Object>) (dt, arguments) -> {
-      List<AST> formalArguments = ((ListExpression) arguments.get(0)).getItems();
-      List<AST> expressions = arguments.subList(1, arguments.size());
-
-      return (BiFunction<DefinitionTable, List<AST>, Object>) (lambdaDt, actualArguments) -> {
-        lambdaDt.enterScope();
-
-        for (int i = 0; i < formalArguments.size(); i++) {
-          String localBindingName = ((Symbol) formalArguments.get(i)).getName();
-          Object localBindingValue = actualArguments.get(i).evaluate(lambdaDt);
-          lambdaDt.bind(localBindingName, localBindingValue);
-        }
-
-        List<Object> evaluatedExpressions = expressions.stream().map(e -> e.evaluate(lambdaDt)).collect(Collectors.toList());
-
-        lambdaDt.leaveScope();
-
-        return evaluatedExpressions.get(evaluatedExpressions.size() - 1);
-      };
-    });
-
-    definitionTable.define("core.concat", (BiFunction<DefinitionTable, List<AST>, Object>) (dt, arguments) -> {
-      List<Object> concatenatedList = new ArrayList<Object>();
-
-      for (AST ast : arguments) {
-        List<Object> item = (List<Object>) ast.evaluate(dt);
-        concatenatedList.addAll(item);
-      }
-
-      return concatenatedList;
-    });
-
-    definitionTable.define("core.repeat", (BiFunction<DefinitionTable, List<AST>, Object>) (dt, arguments) -> {
-      List<Object> repeatedList = new ArrayList<Object>();
-
-      Integer count = (Integer) arguments.get(0).evaluate(dt);
-      Object repeatable = arguments.get(1).evaluate(dt);
-
-      for (int i = 0; i < count; i++) {
-        repeatedList.add(repeatable);
-      }
-
-      return repeatedList;
-    });
-
-    definitionTable.define("core.length", (BiFunction<DefinitionTable, List<AST>, Object>) (dt, arguments) -> {
-      return ((List<Object>) arguments.get(0).evaluate(dt)).size();
-    });
-
-    // Evaluate the AST
-    System.out.println("Evaluated result:");
-    System.out.println(walkReturn.ast.evaluate(definitionTable));
+    PrintableInstructionSequence instructions = app.evaluate(System.in);
+    instructions.printInstructionSequence(new PrintStream(System.out));
   }
 }
 
