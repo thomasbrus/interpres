8861added7159c898cba545013a7011295b6f943
Thomas Brus <thomas.brus@me.com>
2014-06-25 02:20:44 +0200

    Implemented core.list (creates a list from its arguments)

---
+++ src/main/java/interpres/language/definitions/core/List.java
@@ -0,0 +1,29 @@
+package interpres.language.definitions.core;
+
+import java.util.ArrayList;
+
+import interpres.ast.Symbol;
+import interpres.ast.AST;
+
+import interpres.language.definitions.Definition;
+
+import interpres.language.values.Value;
+import interpres.language.values.Lambda;
+
+public class List extends Definition {
+
+  public List() {
+    super("core.list", new Lambda((definitionTable, arguments) -> {
+      java.util.List<Value> listItems = new ArrayList<Value>();
+
+      for (AST argument : arguments) {
+        Value listItemValue = argument.evaluate(definitionTable);
+        listItems.add(listItemValue);
+      }
+
+      return new interpres.language.values.List(listItems);
+    }), 0);
+  }
+
+}
+
