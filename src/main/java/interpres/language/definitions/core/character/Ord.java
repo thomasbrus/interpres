3c7274af3c587250a5c3f7dadcd43220d3d0df27
Thomas Brus <thomas.brus@me.com>
2014-06-25 02:21:19 +0200

    Added core.character.ord (returns ascii value of char)

---
+++ src/main/java/interpres/language/definitions/core/character/Ord.java
@@ -0,0 +1,24 @@
+package interpres.language.definitions.core.character;
+
+import java.util.Arrays;
+
+import interpres.ast.Symbol;
+import interpres.ast.ListExpression;
+
+import interpres.language.definitions.Definition;
+import interpres.language.values.Lambda;
+import interpres.language.values.Character;
+import interpres.language.values.Integer;
+
+public class Ord extends Definition {
+
+  public Ord() {
+    super("core.character.ord", new Lambda((definitionTable, arguments) -> {
+      Character characterValue = (Character) arguments.get(0).evaluate(definitionTable);
+      return new Integer((int) characterValue.getValue().charValue());
+    }), 0);
+  }
+
+}
+
+
