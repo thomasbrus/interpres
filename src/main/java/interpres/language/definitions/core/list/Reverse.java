42cbbb8a1d36be7607a31cc5793aa485853cf09f
Thomas Brus <thomas.brus@me.com>
2014-06-25 02:22:07 +0200

    Implemented core.list.reverse

---
+++ src/main/java/interpres/language/definitions/core/list/Reverse.java
@@ -0,0 +1,24 @@
+package interpres.language.definitions.core.list;
+
+import java.util.Collections;
+
+import interpres.ast.AST;
+import interpres.language.definitions.Definition;
+
+import interpres.language.values.Value;
+import interpres.language.values.Lambda;
+import interpres.language.values.List;
+
+public class Reverse extends Definition {
+
+  public Reverse() {
+    super("core.list.reverse", new Lambda((definitionTable, arguments) -> {
+      List reversableValue = (List) arguments.get(0).evaluate(definitionTable);
+      // FIXME: Shouldn't modify original list (?)
+      Collections.reverse(reversableValue.getItems());
+      return reversableValue;
+    }), 0);
+  }
+
+}
+
