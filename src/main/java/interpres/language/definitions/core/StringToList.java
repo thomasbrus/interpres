2652d1d5b6b0e172e17b13f3273b14dfcc374737
Thomas Brus <thomas.brus@me.com>
2014-06-25 02:19:55 +0200

    Added core.string-to-list

---
+++ src/main/java/interpres/language/definitions/core/StringToList.java
@@ -0,0 +1,33 @@
+package interpres.language.definitions.core;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import interpres.ast.Symbol;
+import interpres.ast.AST;
+
+import interpres.language.definitions.Definition;
+
+import interpres.language.values.Value;
+import interpres.language.values.Lambda;
+import interpres.language.values.Integer;
+import interpres.language.values.String;
+import interpres.language.values.Character;
+
+public class StringToList extends Definition {
+
+  public StringToList() {
+    super("core.string-to-list", new Lambda((definitionTable, arguments) -> {
+      List<Value> characters = new ArrayList<Value>();
+      String stringValue = (String) arguments.get(0).evaluate(definitionTable);
+
+      for (char c : stringValue.getLiteral().toCharArray()) {
+        characters.add(new Character(c));
+      }
+
+      return new interpres.language.values.List(characters);
+    }), 0);
+  }
+
+}
+
