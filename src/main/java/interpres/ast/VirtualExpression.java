ad307d8e3f359612e00069f3beca5392912801db
Thomas Brus <thomas.brus@me.com>
2014-06-25 02:21:57 +0200

    Added virtual expression (used in core.list.map)

---
+++ src/main/java/interpres/ast/VirtualExpression.java
@@ -0,0 +1,21 @@
+package interpres.ast;
+
+import interpres.language.DefinitionTable;
+import interpres.language.values.Value;
+
+public class VirtualExpression extends AST {
+  private Value value;
+
+  public VirtualExpression(Value value) {
+    this.value = value;
+  }
+
+  public Value evaluate(DefinitionTable definitionTable) {
+    return this.value;
+  }
+
+  public Value quote() {
+    return this.value;
+  }
+}
+
