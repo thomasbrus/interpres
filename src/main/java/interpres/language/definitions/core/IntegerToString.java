a71f68263639eb1f14abe2264b29a7c5aedce67d
Thomas Brus <thomas.brus@me.com>
2014-06-25 02:21:00 +0200

    Added core.integer-to-string

---
+++ src/main/java/interpres/language/definitions/core/IntegerToString.java
@@ -0,0 +1,25 @@
+package interpres.language.definitions.core;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import interpres.ast.Symbol;
+import interpres.ast.AST;
+
+import interpres.language.definitions.Definition;
+
+import interpres.language.values.Lambda;
+import interpres.language.values.Integer;
+import interpres.language.values.String;
+
+public class IntegerToString extends Definition {
+
+  public IntegerToString() {
+    super("core.integer-to-string", new Lambda((definitionTable, arguments) -> {
+      Integer integerValue = (Integer) arguments.get(0).evaluate(definitionTable);
+      return new String(integerValue.getValue().toString());
+    }), 0);
+  }
+
+}
+
