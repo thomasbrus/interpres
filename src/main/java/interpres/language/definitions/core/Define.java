a01205a638a5d6a33c57addf1719994cdc2e8d32
Thomas Brus <thomas.brus@me.com>
2014-07-01 13:33:35 +0200

    Always evaluate first argument of core.define

--- src/main/java/interpres/language/definitions/core/Define.java
+++ src/main/java/interpres/language/definitions/core/Define.java
@@ -2,11 +2,10 @@ package interpres.language.definitions.core;
 
 import java.util.Arrays;
 
-import interpres.ast.Symbol;
-
 import interpres.language.definitions.Definition;
 
 import interpres.language.values.Value;
+import interpres.language.values.Symbol;
 import interpres.language.values.Lambda;
 import interpres.language.values.Void;
 
@@ -14,9 +13,9 @@ public class Define extends Definition {
 
   public Define() {
     super("core.define", new Lambda((definitionTable, arguments) -> {
-      Symbol symbol = (Symbol) arguments.get(0);
+      Symbol nameSymbol = (Symbol) arguments.get(0).evaluate(definitionTable);
       Value value = arguments.get(1).evaluate(definitionTable);
-      definitionTable.define(symbol.getName(), value);
+      definitionTable.define(nameSymbol.getIntern(), value);
       return new Void();
     }));
   }
