99b6ba7116fc462174a6d022a711dd08c27d2ad8
Wietze <wietze@users.noreply.github.com>
2014-07-06 00:35:45 +0200

    Implemented array declaration, (indexed) assignment for TAM

--- src/main/java/interpres/language/definitions/core/Bind.java
+++ src/main/java/interpres/language/definitions/core/Bind.java
@@ -1,14 +1,23 @@
 package interpres.language.definitions.core;
 
+import java.util.Collections;
+
+import interpres.ast.AST;
+import interpres.ast.LambdaExpression;
+import interpres.ast.ListExpression;
+import interpres.ast.Symbol;
+
 import interpres.language.definitions.Definition;
-import interpres.language.values.Lambda;
 
 public class Bind extends Definition {
 
   public Bind() {
-    super("core.bind", new Lambda((definitionTable, arguments) ->
-      new interpres.language.invocations.core.Bind(definitionTable, arguments).invoke()
-    ));
+    super("core.bind", new LambdaExpression((definitionTable, arguments) -> {
+      Symbol symbol = (Symbol) arguments.get(0).evaluate(definitionTable);
+      AST value = arguments.get(1).evaluate(definitionTable);
+      definitionTable.set(symbol.getName(), value);
+      return new ListExpression(Collections.emptyList());
+    }));
   }
 
 }
