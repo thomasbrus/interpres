eda1797d4ea8e8d008c85e824907f0a156cad984
Thomas Brus <thomas.brus@me.com>
2014-07-05 03:16:15 +0200

    Added custom RuntimeException that overrides stack trace

---
+++ src/main/java/interpres/language/RuntimeException.java
@@ -0,0 +1,30 @@
+package interpres.language;
+
+import java.util.Stack;
+import interpres.ast.AST;
+
+public class RuntimeException extends java.lang.RuntimeException {
+  private static final long serialVersionUID = 1L;
+  private Stack<StackTraceElement> stackTraceElements = new Stack<StackTraceElement>();
+
+  public RuntimeException(String message) {
+    super(message);
+  }
+
+  public RuntimeException registerFunctionCall(String functionName, String fileName, int lineNumber) {
+    this.stackTraceElements.push(new StackTraceElement(
+      "main", functionName, fileName, lineNumber
+    ));
+
+    return this.updateStackTrace();
+  }
+
+  private RuntimeException updateStackTrace() {
+    this.setStackTrace(this.stackTraceElements.toArray(
+      new StackTraceElement[this.stackTraceElements.size()]
+    ));
+
+    return this;
+  }
+}
+
